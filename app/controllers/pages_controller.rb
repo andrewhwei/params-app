f3bf4d09d35b2d8eb3c8846392af3a6a845e6f7f
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-13 20:35:56 -0700

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController
+
+  def my_name
+    @name = params[:name].upcase
+
+    if @name[0] == "A"
+      @special_message = "Hey, your name starts with the first letter of the alphabet!"
+    end
+  end
+
+  def guessing_game
+    right_number = 36
+    chosen_number = params[:number].to_i
+
+    if chosen_number == right_number
+      @winning_message = "Congrats! You guessed the right number! It was 36!"
+    elsif chosen_number > right_number
+      @high_message = "You guessed too high. Try again!"
+    else
+      @low_message = "You guessed too low. Try again!"
+    end
+  end
+
+end
